447aa595ca30cc50811d2c20cafece49280b24b6
zhaokuirain <zhaokuirain@qq.com>
2020-03-29 21:26:14 +0800

    20200329day1

---
+++ www/app.py
@@ -0,0 +1,21 @@
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+async def index(request):
+	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+
+def init():
+	app = web.Application()
+	app.add_routes([web.get('/', index)])
+	logging.info('server started at http://127.0.0.1...')
+	web.run_app(app, host='127.0.0.1', port=8080)
+
+init()
+
+
+
