400e31e557b4a57748aec568c53fc0bcfe17bca6
zhaokuirain <zhaokuirain@qq.com>
2020-04-03 11:55:04 +0800

    20200403

---
+++ www/syspath.py
@@ -0,0 +1,10 @@
+
+import sys
+import jieba
+print (sys.path)
+# C:\Users\ZK\AppData\Local\Programs\Python\Python39\python39.zip
+# C:\Users\ZK\AppData\Local\Programs\Python\Python39\python.exe
+# C:\ProgramData\Anaconda3\lib\site-packages
+# C:\Users\ZK\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.8_qbz5n2kfra8p0\LocalCache\local-packages\Python38\site-packages
+# C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.8_3.8.752.0_x64__qbz5n2kfra8p0\lib\site-packages
+# C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.8_3.8.752.0_x64__qbz5n2kfra8p0
